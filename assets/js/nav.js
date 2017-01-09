54690c7ab0c8e803f32d76c1c9927f41f8c10a35
David Diomede <diomededavid@gmail.com>
2017-01-08 20:27:19 -0700

    fixed hide

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,7 +1,3 @@
-var nav = document.getElementById('access_nav'),
-    body = document.body;
-
-nav.addEventListener('click', function(e) {
-    body.className = body.className? '' : 'with_nav';
-    e.preventDefault();
+$( ".nav-toggle" ).click(function() {
+  $( "#toggle" ).toggle( "slide" );
 });
